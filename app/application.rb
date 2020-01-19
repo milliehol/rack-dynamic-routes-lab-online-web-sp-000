947844ad21e262954c96c939bf9eae7175fc4827
Carla Holcomb <carla.programmer@gmail.com>
2020-01-19 16:00:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,9 @@ class Application
     if req.path.match(/items/)
  
       items_title = req.path.split("/items/").last 
-      item = @@items.find{|s| s.title == song_title}
+      item = @@items.find{|s| s.name == items_title}
  
-      resp.write {#items.price}
+      resp.write items.price
     end
     else
       resp.write "Route not found"
