3246fbb51ca1eaec6fdfe68a28258ffb6ac5acd5
Carla Holcomb <carla.programmer@gmail.com>
2020-01-19 16:09:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,11 @@
 class Application
   
- 
+  @@items = []
  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-     @@items = []
  
     if req.path.match(/items/)
  
